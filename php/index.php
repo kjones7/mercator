4d8f5c309c03c8f5733d2e5b8e1ebc59af137f10
Kyle Jones <kyleajones242@gmail.com>
2024-05-06 22:53:33 -0400

    Implements a simple map using OpenLayers.

--- php/index.php
+++ php/index.php
@@ -13,3 +13,28 @@ if ($conn) {
     die(print_r(sqlsrv_errors(), true));
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mercator</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/ol@v9.1.0/ol.css">
+</head>
+<body>
+    <div id="map" style="width: 100%; height: 400px"></div>
+    <script src="https://cdn.jsdelivr.net/npm/ol@v9.1.0/dist/ol.js"></script>
+    <script>
+        new ol.Map({
+            layers: [
+                new ol.layer.Tile({source: new ol.source.OSM()}),
+            ],
+            view: new ol.View({
+                center: [0, 0],
+                zoom: 2,
+            }),
+            target: 'map',
+        });
+    </script>
+</body>
+</html>
